1a5c320d82bd13ad303d99c2f12326055decb7fe
TangJicheng <tangjch15@gmail.com>
2024-07-21 15:22:30 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory_resource>
+#include <cstdlib>
+#include <array>
 #include "tracknew.hpp"
 
 int main() {
 	TrackNew::reset();
 
-	std::vector<std::string> vec;
+
+	std::array<std::byte, 200'000> buf;
+	std::pmr::monotonic_buffer_resource pool{buf.data(), buf.size()};
+	std::pmr::vector<std::pmr::string> vec;
+
+
+
+	//std::vector<std::string> vec;
 	//vec.reserve(1001);
 
 	for (size_t i = 0; i < 1000; i++) {
